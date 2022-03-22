19698b4df4f9b48d3d251566dadf0dd52fd56ca1
kkarki <kapil.karki@uni-marburg.de>
2022-03-22 15:25:40 +0100

    tests: implementation of junit test for checking the set headers

--- src/test/java/de/unimarburg/diz/kafkapseudonymizer/ProcessorTests.java
+++ src/test/java/de/unimarburg/diz/kafkapseudonymizer/ProcessorTests.java
@@ -2,6 +2,8 @@ package de.unimarburg.diz.kafkapseudonymizer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.in;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 
 import ca.uhn.fhir.context.FhirContext;
@@ -9,9 +11,12 @@ import org.hl7.fhir.r4.model.Bundle;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
 
@@ -24,11 +29,12 @@ public class ProcessorTests {
 //    private Processor processor;
 
     // 1. Use @ParameterizedTest
-    @Test
-    public void computeOutputTopicFromInputTopic_UsesMatchExpression() {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"fhir-lab","lab-fhir","fhirr-lab", "fhrri-lab",""} )
+    public void computeOutputTopicFromInputTopic_UsesMatchExpression(String testInput) {
         // arrange
         var processor = new Processor(null,"fhir\\-", "psn-fhir-");
-        var testInput = "fhir-lab";
 
         // act
         var result = processor.computeOutputTopicFromInputTopic(testInput);
@@ -37,19 +43,27 @@ public class ProcessorTests {
         assertThat(result.orElseThrow()).isEqualTo("psn-fhir-lab");
     }
 
-    @Test
-    @Disabled
-    public void process_SetsMessageHeaders() {
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"fhir-patient","fhir-lab","","hjff"} )
+    public void process_SetsMessageHeaders(String input) {
         // arrange
         var inputBundle = new Bundle();
+        var inputMessage = new GenericMessage<>(inputBundle);
         var clientMock = Mockito.mock(PseudonymizerClient.class);
         Mockito.when(clientMock.process(any())).thenReturn(new Bundle());
-        var inputMessage = new GenericMessage<>(inputBundle);
-
-        // act
         var processor = new Processor(clientMock,"fhir\\-", "psn-fhir-");
-        var result = processor.process().apply(inputMessage);
-        // TODO
+        var pseudo = clientMock.process(inputMessage.getPayload());
+        var message_with_header = MessageBuilder.withPayload(pseudo)
+            .setHeaderIfAbsent(KafkaHeaders.RECEIVED_TOPIC, input);
+
+        //act
+        var result = processor.process().apply(message_with_header.build());
+        var inputTopic =result.getHeaders().get("spring.cloud.stream.sendto.destination").toString();
+        //assert
+        assertEquals("psn-fhir-patient",inputTopic);
+
     }
 
 }
