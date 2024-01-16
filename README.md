2fa491e2fc73b1e03d7d4ce0a9b432a757899cdd
Sebastian Stöcker <sebastian.stoecker@uni-marburg.de>
2024-01-16 15:18:57 +0100

    docs: move deploy example to README

--- README.md
+++ README.md
@@ -5,6 +5,29 @@ Kafka pseudonymization/anonymization processor featuring a convention based appr
 
 The current implementation supports pseudonymization/anonymization of Kafka messages in FHIR 🔥 format only.
 
+# Deployment
+
+Example via `docker compose`:
+```yml
+kafka-pseudonymizer:
+  image: ghcr.io/diz-unimr/kafka-pseudonymizer:latest
+  restart: unless-stopped
+  environment:
+    BOOTSTRAP_SERVERS: localhost:9092
+    SECURITY_PROTOCOL: SSL
+    INPUT_TOPICS: "^(?=.*-fhir)(?!.*fhir-psn).*"
+    INPUT_IS_PATTERN: "true"
+    CONSUMER_THREADS: 6
+    SERVICES_KAFKA_OUTPUT_TOPIC_MATCH_EXPRESSION: fhir-idat
+    SERVICES_KAFKA_OUTPUT_TOPIC_REPLACE_WITH: fhir-psn
+    SERVICES_PSEUDONYMIZER_URL: http://localhost:5000/fhir
+    SSL_KEY_STORE_PASSWORD: private-key-password
+    SSL_TRUST_STORE_PASSWORD: store-password
+  volumes:
+    - ./cert/keystore.jks:/opt/kafka-pseudonymizer/ssl/keystore.jks:ro
+    - ./cert/truststore.jks:/opt/kafka-pseudonymizer/ssl/truststore.jks:ro
+```
+
 ## License
 
 [AGPL-3.0](https://www.gnu.org/licenses/agpl-3.0.en.html)
--- src/intTest/java/de/unimarburg/diz/kafkapseudonymizer/IntegrationTests.java
+++ src/intTest/java/de/unimarburg/diz/kafkapseudonymizer/IntegrationTests.java
@@ -27,7 +27,8 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {"services.kafka.generate-output-topic.match-expression=fhir",
+@TestPropertySource(properties = {
+    "services.kafka.generate-output-topic.match-expression=fhir",
     "services.kafka.generate-output-topic.replace-with=fhir-psn",
     "spring.cloud.stream.bindings.process-in-0.destination=test-fhir(?!-psn).*",
     "spring.cloud.stream.kafka.bindings.process-in-0.consumer.destinationIsPattern=true"})
@@ -36,7 +37,8 @@ class IntegrationTests extends TestContainerBase {
 
     @DynamicPropertySource
     private static void kafkaProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.kafka.bootstrapServers", kafka::getBootstrapServers);
+        registry.add("spring.kafka.bootstrapServers",
+            kafka::getBootstrapServers);
         registry.add("services.pseudonymizer.url",
             () -> "http://" + fhirPseudonymizer.getHost() + ":"
                 + fhirPseudonymizer.getFirstMappedPort() + "/fhir");
@@ -47,13 +49,14 @@ class IntegrationTests extends TestContainerBase {
         setup();
 
         // produce message to input topic
-        // note: this creates the input topic which must exist before the kafka binder
-        // resolves the input topic expression
+        // note: this creates the input topic which must exist before the
+        // kafka binder resolves the input topic expression
         var producer = createProducer();
 
         // create input bundle
         var bundle = new Bundle().addEntry(
-            new BundleEntryComponent().setResource(new Patient().setId("123456")));
+            new BundleEntryComponent().setResource(
+                new Patient().setId("123456")));
         var record = new ProducerRecord<String, Bundle>("test-fhir", bundle);
         // .. and send
         producer.send(record);
@@ -61,10 +64,13 @@ class IntegrationTests extends TestContainerBase {
 
     private static KafkaProducer<String, Bundle> createProducer() {
         var producerProps = new HashMap<String, Object>();
-        producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
+        producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+            kafka.getBootstrapServers());
         producerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "test-producer");
-        producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaFhirSerializer.class);
+        producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+            StringSerializer.class);
+        producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+            KafkaFhirSerializer.class);
         return new KafkaProducer<>(producerProps);
     }
 
@@ -74,16 +80,18 @@ class IntegrationTests extends TestContainerBase {
         var outputTopic = "test-fhir-psn";
         var consumer = createConsumer();
         consumer.subscribe(List.of(outputTopic));
-        var consumed = KafkaTestUtils.getSingleRecord(consumer, outputTopic, Duration
-            .ofMinutes(2)
-            .toMillis());
+        var consumed = KafkaTestUtils.getSingleRecord(consumer, outputTopic,
+            Duration
+                .ofMinutes(2)
+                .toMillis());
 
         var pseudonymizedPatient = (Patient) consumed
             .value()
             .getEntryFirstRep()
             .getResource();
 
-        var pseudedCoding = new Coding("http://terminology.hl7.org/CodeSystem/v3-ObservationValue",
+        var pseudedCoding = new Coding(
+            "http://terminology.hl7.org/CodeSystem/v3-ObservationValue",
             "PSEUDED", "pseudonymized");
 
         assertThat(pseudonymizedPatient)
@@ -96,9 +104,11 @@ class IntegrationTests extends TestContainerBase {
 
     private KafkaConsumer<String, Bundle> createConsumer() {
         var consumerProps = new HashMap<String, Object>();
-        consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
+        consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+            kafka.getBootstrapServers());
         consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
-        consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+            StringDeserializer.class);
         consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
             KafkaFhirDeserializer.class);
         consumerProps.put(ConsumerConfig.CLIENT_ID_CONFIG, "test-consumer");
